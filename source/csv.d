98e2592bd586c0a497e9fa4211b9596e6d2ef4a7
cgregory <charles.gregory940@topper.wku.edu>
2019-01-14 20:20:41 -0500

    fix column issue

--- source/csv.d
+++ source/csv.d
@@ -35,7 +35,7 @@ struct Table {
         //create records
         foreach(line;lines){
             auto split=line.splitter(delim);
-            auto rec=split.take(startSamples-1).array;
+            auto rec=split.take(startSamples).array;
             records~=Record(rec);
         }
         matrix=createMatrix();
