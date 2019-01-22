4b8ef9873887c9b542e38b23d4a00a8b89668abd
cgregory <charles.gregory940@topper.wku.edu>
2019-01-22 15:38:29 -0500

    now accepts threads parameter

--- source/app.d
+++ source/app.d
@@ -6,15 +6,29 @@ import std.algorithm:map,count;
 import std.range:array;
 import std.path:buildPath;
 import std.conv:to;
-import std.parallelism:parallel;
+import std.parallelism:parallel,defaultPoolThreads;
+import std.getopt;
 import dhtslib.sam:SAMFile;
+
+int threads=0;
 
 import csv;
 void main(string[] args)
 {
+	auto res=getopt(args,config.bundling,
+	"threads|t","threads for running depth gauge",&threads);
+	if (res.helpWanted) {
+		defaultGetoptPrinter("usage: ./depthgauge [input tsv] [number of first sample column] [bam folder] [output tsv]", res.options);
+		stderr.writeln();
+		return;
+	}
 	if(args.length!=5){
 		writeln("usage: ./depthgauge [input tsv] [number of first sample column] [bam folder] [output tsv]");
+		return;
 	}else{
+		if(threads!=0){
+			defaultPoolThreads(threads);
+		}
 		auto t =Table(args[1],args[3],args[2].to!int-1);
 		getDepths(t,args[3]);
 		File f = File(args[4],"w");
